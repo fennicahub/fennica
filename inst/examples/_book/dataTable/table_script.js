352d70d474d620459b11197d8f29572df026607f
NRV2ouf <teorock76@yahoo.fr>
2023-05-29 11:49:39 +0300

    Finished version of dataTable

--- inst/examples/_book/dataTable/table_script.js
+++ inst/examples/_book/dataTable/table_script.js
@@ -1,27 +1,37 @@
-
 let dTable;
 let size;
-window.addEventListener('DOMContentLoaded', init);
+let columnCount;
 
-/** TODO
- * Check if file exists (~parse text result)
- * Only load a part of bigger files and send a notification to user if the file is trimmed.
-*/
+import Toast from "./toastNotification/Toast.js";
+
+window.addEventListener('DOMContentLoaded', init);
 
 function init() {
     const path = getPathFromURL();
     const maxSize = 5000000; // 5MB
 
-    setFile(path);
-    displayFileSize(path)
-    .then(() => {
-        if (size <= maxSize){
-            createTable(path);
+
+    (async (path) => {
+        const exists = await fileExists(path);
+      
+        if (exists) {
+            setFile(path);
+            displayFileSize(path)
+            .then(() => {
+                if (size <= maxSize){
+                    createTable(path);
+                } else {
+                    createTablePart(path, maxSize);
+                    toastIncompleteFile();
+                    specifyIsPreview();
+                }
+            });
         } else {
             hideLoader();
-            window.alert("This file is too heavy for displaying...\nBut you can download it from the top right corner !")
+            toastFileDoesntExist();
+            fileNotFound();
         }
-    });
+    })(path);
 }
 
 function hideLoader(){
@@ -52,7 +62,7 @@ function setFile(path){
 
 function setFileSize(fileSize){
     size = fileSize;
-    span = document.getElementById('fileSize');
+    let span = document.getElementById('fileSize');
     if (fileSize >= 1000){
         fileSize = Math.round(fileSize / 1000 * 10) / 10;
         if (fileSize >= 1000){
@@ -76,6 +86,24 @@ function setFileSize(fileSize){
     }
 }
 
+function fileNotFound(){
+    const strFileNotFound = "File not found";
+    const fileName = document.getElementById('fileName');
+
+    document.title = strFileNotFound;
+    fileName.textContent = strFileNotFound;
+    fileName.style.color = 'RED';
+    document.getElementById('fileSize').textContent = "Null";
+}
+
+function specifyIsPreview(){
+    const fileName = document.getElementById('fileName');
+    var span = document.createElement("span");
+    span.style.color = 'RED';
+    span.textContent = 'Preview of '
+    fileName.prepend(span);
+}
+
 /***** Fetch URL parameter **********************/
 /************************************************/
 
@@ -83,11 +111,50 @@ function getPathFromURL(){
     var url_string = window.location.href;
     var url = new URL(url_string);
     var path = url.searchParams.get("path");
+
+    
     return path
 }
 
 /***** Fetch CSV data and create DataTable ******/
 /************************************************/
+
+async function fileExists(filePath) {
+    if(!filePath){
+        return false;
+    }
+    try {
+        const response = await fetch(filePath);
+        return response.ok;
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
+    }
+}
+
+async function fetchPartialFile(url, maxSizeInBytes) {
+    const rangeEnd = maxSizeInBytes - 1;
+    const headers = { Range: `bytes=0-${rangeEnd}` };
+  
+    const response = await fetch(url, { headers });
+    const blob = await response.blob();
+    
+    return blobToString(blob);
+}
+
+function blobToString(blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+  
+      reader.onloadend = () => {
+        const text = reader.result;
+        resolve(text);
+      };
+  
+      reader.onerror = reject;
+      reader.readAsText(blob);
+    });
+  }
 
 function createTable(filePath) {
   fetch(filePath)
@@ -95,22 +162,35 @@ function createTable(filePath) {
         return response.text();
     })
     .then(function(data) {
-        CSVToTable(data);
+        var rows = data.trim().split(/\r?\n|\r/); 
+        rowsToTable(rows);
     })
     .then(function() {
         initializeDataTable();
     });
 }
 
-function CSVToTable(csvString) {
+function createTablePart(filePath, maxSize) {
+    fetchPartialFile(filePath, maxSize)
+    .then(function(data) {
+        var rows = data.trim().split(/\r?\n|\r/); 
+        rows.pop();
+        rowsToTable(rows);
+    })
+    .then(function() {
+        initializeDataTable();
+    });
+  }
+
+function rowsToTable(rows) {
     // split the CSV rows
-    var rows = csvString.trim().split(/\r?\n|\r/); 
     
     if(rows.length > 0){
         var headerColumns = rows.shift().split('\t');
-        headerRow = document.createElement('tr');
+        columnCount = headerColumns.length;
+        var headerRow = document.createElement('tr');
         headerColumns.forEach(columnTitle => {
-            th = document.createElement('th');
+            var th = document.createElement('th');
             th.textContent = columnTitle;
             headerRow.append(th);
         });
@@ -122,12 +202,17 @@ function CSVToTable(csvString) {
 
         rows.forEach(row => {
             var data = row.split(/\t/);
-            dataRow = document.createElement('tr');
-            data.forEach(element => {
-                td = document.createElement('td');
+            var dataRow = document.createElement('tr');
+
+            for(var i = 0 ; i < columnCount ; i++){
+                var element = "";
+                if (i < row.length){
+                    element = data[i];
+                }
+                var td = document.createElement('td');
                 td.textContent = element;
                 dataRow.append(td);
-            });
+            };
             dataTableBody.appendChild(dataRow);
         });
     }
@@ -142,4 +227,51 @@ function initializeDataTable() {
     });
 
     hideLoader();
+}
+
+/***** Toast notifications **********************/
+/************************************************/
+
+function toastIncompleteFile(){
+    new Toast({
+        autoClose: 30000, // 30 seconds
+        canCloseOnClick: true,
+        darkMode: false,
+        // callback function
+        onClose: () => {},
+        
+        pauseOnHover: true,
+        pauseOnFocusLoss: true,
+        playNotificationSound: false,
+        showProgressBar: true,
+
+        toastContent: {
+            title: "Incomplete file..",
+            message: "The file is too big for viewing.\nAlthough you can download it from the top right corner !",
+            type: "warning",
+        },
+        position: "bottom-right",
+    });
+}
+
+function toastFileDoesntExist(){
+    new Toast({
+        autoClose: 900000, // 15 minutes
+        canCloseOnClick: true,
+        darkMode: false,
+        // callback function
+        onClose: () => {},
+        
+        pauseOnHover: true,
+        pauseOnFocusLoss: true,
+        playNotificationSound: false,
+        showProgressBar: false,
+
+        toastContent: {
+            title: "file not found..",
+            message: "Unfortunately, the file you are looking for doesn't exist...",
+            type: "error",
+        },
+        position: "bottom-right",
+    });
 }
