1d15c3caf03e47b9693759087f55096f1eeb2b6e
NRV2ouf <96051310+NRV2ouf@users.noreply.github.com>
2023-06-06 16:23:58 +0300

    Update extract-files.js

--- .github/workflows/extract-files.js
+++ .github/workflows/extract-files.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const yaml = require('js-yaml')
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-const bookdownConfig = fs.readFileSync('inst/examples/_bookdown.yml', 'utf-8')
-const rmdFiles = yaml.load(bookdownConfig).rmd_files
-const files = rmdFiles.map(file => `inst/examples/${file}`)
+const bookdownConfig = fs.readFileSync('inst/examples/_bookdown.yml', 'utf-8');
+const rmdFiles = yaml.load(bookdownConfig).rmd_files;
+const files = rmdFiles.map(file => `inst/examples/${file.replace('.Rmd', '.html')}`);
 
-console.log(files.join(' '))
+console.log(files.join(' '));
