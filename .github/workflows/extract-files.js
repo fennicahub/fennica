7ff56150d314e3d95cca8808b31a2b085a132ce8
NRV2ouf <teorock76@yahoo.fr>
2023-06-08 16:04:19 +0300

    revert tests on workflows

--- .github/workflows/extract-files.js
+++ .github/workflows/extract-files.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
+const fs = require('fs')
+const yaml = require('js-yaml')
 
-const bookdownConfig = fs.readFileSync('inst/examples/_bookdown.yml', 'utf-8');
-const rmdFiles = yaml.load(bookdownConfig).rmd_files;
-const files = rmdFiles.map(file => `inst/examples/_book/${file.replace('.Rmd', '.html')}`);
-console.log(files.join(' '));
+const bookdownConfig = fs.readFileSync('inst/examples/_bookdown.yml', 'utf-8')
+const rmdFiles = yaml.load(bookdownConfig).rmd_files
+const files = rmdFiles.map(file => `inst/examples/${file}`)
+
+console.log(files.join(' '))
