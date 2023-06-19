16f3019a8c81e5a2b6847df5888008a4709fe23b
NRV2ouf <teorock76@yahoo.fr>
2023-06-19 10:46:53 +0300

    Add the R function harmonize_field_names

---
+++ R/harmonize_field_names.R
@@ -0,0 +1,24 @@
+#' @title Harmonize field names
+#' @description Convert field codes to names 
+#' @param x a vector of field codes
+#' @return Vector of field names
+#' @export
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @references See citation("comhis")
+#' @examples x2 <- harmonize_field_names("041a")
+#' @details For MARC field descriptions, see \url{https://www.loc.gov/marc/bibliographic/}.
+#' @keywords utilities
+harmonize_field_names <- function (x = NULL) {
+
+  # Get printing terms from a table
+  f <- system.file("extdata/fieldnames.csv", package = "comhis")
+  map <- read.csv(f, sep = "|")
+
+  if (!is.null(x)) {
+    ret <- map[match(x, map$field), "name"]
+  } else {
+    ret <- map
+  }
+  ret
+}
+
