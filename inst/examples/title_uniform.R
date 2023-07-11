328b98c8dfc6030f328a510087ba6a1cf70b5d2d
Julia <yulmat@utu.fi>
2023-07-11 08:33:45 +0000

    ok

--- inst/examples/title_uniform.R
+++ inst/examples/title_uniform.R
@@ -1,16 +1,16 @@
 
 field <- "title_uniform"
-df.tmp <- data.frame(df.orig.full[[field]])
+df.tmp <- data.frame(df.orig[[field]])
 names(df.tmp) <- field
 
 # Raw data
-original <- df.orig.full[[field]]
+original <- df.orig[[field]]
 
 # Harmonize the raw data
 x <- fennica::polish_title(original)
 
 # Collect the results into a data.frame
-df.tmp <- data.frame(melinda_id = df.orig.full$melinda_id,
+df.tmp <- data.frame(melinda_id = df.orig$melinda_id,
                      title_uniform = x)
 
 # Store the title field data
@@ -35,7 +35,7 @@ message("Discarded entries in the original data")
 inds <- which(is.na(df.tmp[[field]]))
 
 # Original entries that were converted into NA
-original.na <- df.orig.full[match(df.tmp$melinda_id[inds], df.orig.full$melinda_id), field]
+original.na <- df.orig[match(df.tmp$melinda_id[inds], df.orig$melinda_id), field]
 
 # .. ie. those are "discarded" cases; list them in a table
 tmp <- write_xtable(original.na, file_discarded, count = TRUE)
