bce8204d6165dca01d04e9d2379cf31fd1e13097
Julia <yulmat@utu.fi>
2023-10-03 17:29:50 +0000

    physical extent update

--- inst/examples/physical_extent.R
+++ inst/examples/physical_extent.R
@@ -1,9 +1,43 @@
 
 field <- "physical_extent"
-df.tmp <- bibliographica::polish_physical_extent(df.orig[[field]], verbose = TRUE)
+df.tmp <- polish_physical_extent(df.orig[[field]], verbose = TRUE)
 rm.dim.file <- "remove_dimension.csv"
 df.tmp <- polish_physical_extent(df.orig[[field]], verbose = TRUE, rm.dim.file = rm.dim.file)
 
 # Vol number and count fields could not be extracted from physical_extent field in Fennica - remove
 df.tmp$volcount <- NULL
 df.tmp$volnumber <- NULL
+
+# Store the title field data
+# FIXME: convert to feather or plain CSV
+data.file <- paste0(field, ".Rds")
+saveRDS(df.tmp, file = data.file)
+
+# Define output files
+file_accepted  <- paste0(output.folder, field, "_accepted.csv")
+file_discarded <- paste0(output.folder, field, "_discarded.csv")
+
+# ------------------------------------------------------------
+
+# Generate data summaries
+
+message("Accepted entries in the preprocessed data")
+s <- write_xtable(df.tmp[[field]], file_accepted, count = TRUE)
+
+message("Discarded entries in the original data")
+
+# NA values in the final harmonized data
+inds <- which(is.na(df.tmp[[field]]))
+
+# Original entries that were converted into NA
+original.na <- df.orig[match(df.tmp$melinda_id[inds], df.orig$melinda_id), field]
+
+# .. ie. those are "discarded" cases; list them in a table
+tmp <- write_xtable(original.na, file_discarded, count = TRUE)
+
+# ------------------------------------------------------------
+
+# Generate markdown summary 
+df <- readRDS(data.file)
+# tmp <- knit(input = paste(field, ".Rmd", sep = ""), 
+#             output = paste(field, ".md", sep = ""))
