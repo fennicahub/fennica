6dc168c32768456e616aa29a27a643bc5f64054e
Julia <yulmat@utu.fi>
2023-10-18 11:34:32 +0000

    trimws

--- inst/examples/funcs.R
+++ inst/examples/funcs.R
@@ -2845,7 +2845,7 @@ remove_print_statements <- function (x) {
 
   ### Get printing terms from tables in various languages
   for (lang in c('finnish','english', 'french', 'german', 'swedish')) {
-    terms <- unique(str_trim(tolower(read.csv(paste("printstop_",lang,".csv"), stringsAsFactors = FALSE)[,1])))
+    terms <- unique(str_trim(tolower(read.csv(trimws(paste0("printstop_",lang,".csv")), stringsAsFactors = FALSE)[,1])))
     # Harmonize the terms 
     terms.multi <- c(terms.multi, terms[nchar(terms) > 1])
     terms.single <- c(terms.single, terms[nchar(terms) == 1])
