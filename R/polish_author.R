472c0ecf72b401347be09fa95ffcbcf63d6482a1
Julia <yulmat@utu.fi>
2023-10-31 10:17:15 +0000

    polish year change

--- R/polish_author.R
+++ R/polish_author.R
@@ -17,24 +17,24 @@ polish_author <- function (s, stopwords = NULL, verbose = FALSE) {
     # TODO Use instead the notnames function here ?
     
     #f <- system.file("extdata/stopwords.csv")
-    stopwords.general <- as.character(read.csv("extdata/stopwords.csv", sep = "\t")[,1])
+    stopwords.general <- as.character(read.csv("stopwords.csv", sep = "\t")[,1])
     stopwords.general <- c(stopwords.general, stopwords(kind = "en"))
 
     #f <- system.file("extdata/stopwords_for_names.csv")
-    stopwords.names <- as.character(read.csv("extdata/stopwords_for_names.csv", sep = "\t")[,1])
+    stopwords.names <- as.character(read.csv("stopwords_for_names.csv", sep = "\t")[,1])
 
     #f <- system.file("extdata/organizations.csv")
-    stopwords.organizations <- as.character(read.csv("extdata/organizations.csv", sep = "\t")[,1])
+    stopwords.organizations <- as.character(read.csv("organizations.csv", sep = "\t")[,1])
     
     #f <- system.file("extdata/stopwords_titles.csv")
-    stopwords.titles <- as.character(read.csv("extdata/organizations.csv", sep = "\t")[,1])
+    stopwords.titles <- as.character(read.csv("organizations.csv", sep = "\t")[,1])
     stopwords <- unique(c(stopwords.general, stopwords.organizations, stopwords.names, stopwords.titles))
   }
 
   # Accept some names that may be on the stopword lists
   # TODO add here all known names
   #f <- system.file("extdata/author_accepted.csv")
-  author.accepted <- as.character(read.csv("extdata/author_accepted.csv", sep = "\t")[,1])
+  author.accepted <- as.character(read.csv("author_accepted.csv", sep = "\t")[,1])
   pseudo <- get_pseudonymes()  
   accept.names <- unique(c(pseudo, author.accepted))
   # Also add individual terms in these names on the list
