87da8b619f37ff50a272d9aa380d2aee7c8db91d
Julia <yulmat@utu.fi>
2023-11-08 12:39:31 +0000

    funcs change

--- inst/examples/funcs.R
+++ inst/examples/funcs.R
@@ -6726,7 +6726,14 @@ polish_author <- function (s, stopwords = NULL, verbose = FALSE) {
     if (!is.na(la)) {    
       la <- unlist(strsplit(la, " "), use.names = FALSE)
     }
+    if (length(fi) == 0) {fi <- NA}
+    if (length(la) == 0) {la <- NA}    
     
+    #if (!is.na(fi) && !is.na(la)) {
+      #if (la == fi[[length(fi)]]) {
+        #fi <- fi[-length(fi)]
+      #}
+    }
     first[[i]] <- paste(fi, collapse = " ")
     last[[i]] <- paste(la, collapse = " ")    
   }
