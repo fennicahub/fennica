2a466202112bee339c7c016c08d6cae6636d4ee5
Julia <yulmat@utu.fi>
2024-02-21 16:25:25 +0000

    publication status

--- inst/examples/list.R
+++ inst/examples/list.R
@@ -8,25 +8,9 @@ library(ggplot2)
 library(Cairo)
 library(fennica) 
 source("funcs.R")
-
-unzip("the_list.zip",exdir=".")
-df_list <- read.csv(file = "the_list.csv", skip = 1, head = TRUE, sep="\t")
+#source("publication_time.R")
 
 
-#rename column names
-df_list <- df_list %>% 
-  dplyr::rename("melinda_id" = 1, #("035","a")
-                "author_name" = 2, # ("100","a")
-                "author_date" = 3, # ("100","d")
-                "publication_time" = 4, #("260","c")
-                "language" = 5, # ("041","a")
-                "language_original" = 6, # ("041","h")
-                "title" = 7, #("245","a")
-                "title_remainder" = 8, #("245","b")
-                "udc" = 9, #("080","a")  Universal Decimal Classification number (NR)
-                "udc_aux" = 10, #("080","x") Common auxiliary subdivision (R)
-                "edition"= 11, # ("250","a"),
-                "signum" = 12) # ("callnumbers","a")
 
 # Parameters: 
 # 0) ("260","c") 1809-1917
@@ -34,36 +18,52 @@ df_list <- df_list %>%
 #publication_time field in df.orig
 
 melindas <- df_pubtime19$melinda_id
-list <-  df_list[df_list$melinda_id %in% melindas, ]
-
-list$edition <- gsub("Lyh. laitos,", "", list$edition)
-
+list <-  df.orig[df.orig$melinda_id %in% melindas, ]
 
 # 5) 250 a (tyhjä) (only this gives 47627)
-
-list <- list[is.na(list$edition) | list$edition == "" |  list$edition == "1. p.", ]
+list <- list[is.na(list$"250a") | list$"250a" == "", ]
 
 
 # 2) 041 h (tyhjä) (43106)
 
 list <- list[is.na(list$language_original) | list$language_original == "", ]
 
-# 1) 041 a fin ja swe (47160)
+# 1) 041 a fin ja (tai) swe (47160)
 
-list <- list[grepl("fin|swe", list$language), ]
+list <- list[grepl("fin|swe", list$language) | is.na(list$language), ]
 
 
-# 3) 080 a 839.79 ja 894.541 (5124)
+# 3) 080 a 839.79 ja (tai) 894.541 (5124)
 
-list <- list[grepl("839.79|894.541", list$udc), ]
+list <- list[grepl("839.79|839.7|894.541|820/89|894.54", list$UDK)| is.na(list$UDK), ]
 
 
 # 4) 080 x -1, -2, -3, -4 (only this parameter gives 4993 obs)
 
-list <- list[
-              list$udc_aux == "-1" | 
-              list$udc_aux == "-2" |
-              list$udc_aux == "-3" |
-              list$udc_aux == "-4",
-             ]
+
+# Assuming list is your data frame and "080x" is the column name
+# The pattern is a regular expression that matches -1, -2, -3, or -4
+
+list <- list[grepl("-1|-2|-3|-4", list$'080x') | is.na(list$'080x'), ]
+
+# genre based search 
+
+
+
+# # Define the genres you want to keep
+genres_to_keep <- c("Fiction (not further specified)", "Dramas", "Essays", "Novels",  
+                    "Humor, satires, etc.", "Short stories, stories or their collections",  
+                    "Combination of genres", "Poems")
+
+# # Filter the list DataFrame to keep only the rows with the specified genres
+list <- list %>% filter(genre_book %in% genres_to_keep)
+
+#kaunokirj.
+
+#list <- list[grepl("kauno", list$signum) | is.na(list$signum), ]
+
+############################################################################
+
+#the list has only language material type of record 
+unique_counts <- table(list$type_of_record)
 
