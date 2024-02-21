7ed277a0ab082708192bb86d2530ca6f1c7a2c4e
Julia <yulmat@utu.fi>
2024-02-21 13:56:52 +0000

    author name check

--- inst/examples/content_type.R
+++ inst/examples/content_type.R
@@ -1,2 +1,19 @@
-field <- "type_of_record"
+library(dplyr)
 
+unique_count <- as.data.frame(df.orig %>%
+  summarise(n_unique = n_distinct(type_of_record)))
+
+unique_counts <- table(df.orig$type_of_record)
+
+unique_counts_df$percentage <- unique_counts_df$n_unique / sum(unique_counts_df$n_unique) *  100
+
+
+
+
+top_plot(df.orig, "type_of_record", ntop = 10, log10 = TRUE) + labs(y = "Entries (n)")
+
+
+x <- top(df.orig, "type_of_record")
+tab <- cbind(names(x), unname(x), round(100 * unname(x/nrow(df)), 1))
+colnames(tab) <- c("Type of Record", "Documents (n)", "Fraction (%)")
+kable(head(tab, 14))
--- inst/examples/velka_list.R
+++ inst/examples/velka_list.R
@@ -1,21 +1,25 @@
 
-install.packages("qdapRegex")
+#install.packages("qdapRegex")
 library(qdapRegex)
 
-install.packages("readxl")
+#install.packages("readxl")
 library(readxl)
 
 # Read Excel file
 velka_list <- read_excel("Kirjallisuus.xlsx")
+# Change the column name using colnames
+colnames(velka_list)[colnames(velka_list) == "MelindaID"] <- "melinda_id"
+
 
 
 # change FCC to FI-MELINDA to match df.orig melindas 
-velka_list$MelindaID <- gsub(".*FCC", "FCC", velka_list$MelindaID)
-velka_list$MelindaID <- sub("FCC", "", velka_list$MelindaID)
-velka_list$MelindaID <- sub(" ", "", velka_list$MelindaID)
-velka_list$MelindaID <- sub("(FI-MELINDA)", "", velka_list$MelindaID)
-#velka_list$MelindaID <- rm_bracket(velka_list$MelindaID, pattern = "(", replacement = "")
-#velka_list$MelindaID <- rm_bracket(velka_list$MelindaID, pattern = ")", replacement = "")
+velka_list$melinda_id <- gsub(".*FCC", "FCC", velka_list$melinda_id)
+velka_list$melinda_id <- sub("FCC", "", velka_list$melinda_id)
+velka_list$melinda_id <- sub(" ", "", velka_list$melinda_id)
+velka_list$melinda_id <- sub("(FI-MELINDA)", "", velka_list$melinda_id)
+
+velka_list$melinda_id <- substr(velka_list$melinda_id, start =  1, stop =  9)
+
 
 
 #copy list df to this file for testing stuff
@@ -26,17 +30,23 @@ melindas_filtered$melinda_id <- gsub(".*\\(FI-MELINDA\\)", "\\(FI-MELINDA\\)", m
 melindas_filtered$melinda_id <- gsub("\\(FI-MELINDA\\)", "", melindas_filtered$melinda_id)
 melindas_filtered$melinda_id <- sub("FCC", "", melindas_filtered$melinda_id)
 melindas_filtered$melinda_id <- sub(" ", "", melindas_filtered$melinda_id)
-#melidas_filtered$melinda_id <- rm_bracket(melidas_filtered$melinda_id, pattern = "(", replacement = "")
-#melidas_filtered$melinda_id <- rm_bracket(melidas_filtered$melinda_id, pattern = ")", replacement = "")
+
+all_melindas <- df.orig
+all_melindas$melinda_id <- gsub(".*\\(FI-MELINDA\\)", "\\(FI-MELINDA\\)", all_melindas$melinda_id)
+all_melindas$melinda_id <- gsub("\\(FI-MELINDA\\)", "", all_melindas$melinda_id)
+all_melindas$melinda_id <- sub("FCC", "", all_melindas$melinda_id)
+all_melindas$melinda_id <- sub(" ", "", all_melindas$melinda_id)
+
+
 
 
 # Get the unique values from both columns
-unique_values_VL <- unique(velka_list$MelindaID)
-unique_values_L <- unique(melindas_filtered$melinda_id)
+VL <- velka_list$melinda_id
+L <- melindas_filtered$melinda_id
 
 # Find the values that are present in VL but not in L
-non_matching_values_VL <- setdiff(unique_values_VL, unique_values_L)
-non_matching_values_L <- setdiff(unique_values_L, unique_values_VL)
+non_matching_values_VL <- setdiff(VL, L)
+non_matching_values_L <- setdiff(L, VL)
 
 
 # Print the non-matching values
@@ -44,11 +54,33 @@ print(non_matching_values_VL)
 print(non_matching_values_L)
 
 
-matches <- match(unique_values_VL, unique_values_L)
-
 # Identify the values in VL that have a match in L
-matching_values <- unique_values_VL[!is.na(matches)]
+matching_values <- VL[!is.na(matches)]
 
 # Print the matching values
 print(matching_values)
+
+library(dplyr)
+
+non_fennica <- setdiff(non_matching_values_VL, all_melindas$melinda_id)
+
+not_on_julia_list <- setdiff(non_matching_values_VL, non_fennica)
+print(not_on_julia_list)
+
+
+# Assuming all_melindas is your data frame and not_on_julia_list is your list of values
+
+# Filter the data frame to include only rows where melinda_id is in the not_on_julia_list
+not_on_julia_list <- all_melindas[all_melindas$melinda_id %in% not_on_julia_list, ]
+
+# Print the chunk
+print(matching_chunk) # the whole thing is rejected by polish_years 
+                      # it is probably in the discraded publication time file
+
+
+# Count the number of matching melinda_id values
+count_matching <- sum(matching_chunk$melinda_id %in% df.orig$melinda_id)
+
+# Print the count
+print(count_matching)
 
