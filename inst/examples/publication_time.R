e9cf85c1fc6571540871a4a2e0f1b7a95e11426e
Julia <yulmat@utu.fi>
2024-03-13 13:10:38 +0000

    add df_pub_time

--- inst/examples/publication_time.R
+++ inst/examples/publication_time.R
@@ -19,14 +19,16 @@ df.harmonized$publication_decade <- decade(df.harmonized$publication_year)
 
 # ------------------------------------------------------------
 
-# # Store the publication time field data
-# data.file <- paste0(field, ".Rds")
-# saveRDS(df.harmonized, file = data.file)
-# #Load the RDS file
-# df <- readRDS(data.file)
-# # Convert to CSV and store in the data.files folder
-# #write.csv(df, gzfile(paste0("data.files/", paste0(field, ".csv.gz")), "w"), row.names = FALSE)
-# 
+# Store the publication time field data
+data.file <- paste0(field, ".Rds")
+saveRDS(df.harmonized, file = data.file)
+# Generate markdown summary for the whole data
+df_pub_time <- readRDS(data.file)
+#Load the RDS file
+#df <- readRDS(data.file)
+# Convert to CSV and store in the data.files folder
+#write.csv(df, gzfile(paste0("data.files/", paste0(field, ".csv.gz")), "w"), row.names = FALSE)
+
 
 
 # ---------------------------------------------------------------------
