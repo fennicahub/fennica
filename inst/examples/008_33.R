1fe49a1edfece74172855c8a3ef8996224d58988
Julia <yulmat@utu.fi>
2025-01-29 12:17:10 +0000

    title update

--- inst/examples/008_33.R
+++ inst/examples/008_33.R
@@ -5,7 +5,8 @@ field <- `008_33`
 # Select only the 008 columns from df.orig
 df.tmp <- df.orig %>%
   select(
-    melinda_id, 
+    melinda_id,
+    leader,
     `008`, 
     type_of_record, 
     bibliographic_level, 
@@ -20,6 +21,8 @@ df.tmp1 <- df.tmp %>%
 df.tmp2 <- df.tmp %>%
   filter(!is.na(converted_008_33))
 
+# Filter rows where data_element_008 == "Kirjat" and converted_00_33 is NA
+filtered_kirja <- df.tmp[df.tmp$data_element_008 == "Kirjat" & is.na(df.tmp$converted_00_33), ]
 
 # Define output files
 file_accepted  <- paste0(output.folder, field, "_accepted.csv")
@@ -27,10 +30,50 @@ file_discarded <- paste0(output.folder, field, "_discarded.csv")
 file_discarded_id <- paste0(output.folder, field, "_discarded_id.csv")
 
 # ------------------------------------------------------------
+print(df.tmp$data_element_008))
+sum(is.na(df.tmp$data_element_008))
 
-# Generate data summaries for the whole dataset
+counts <- table(df.orig$data_element_008, useNA = 'always')
+
+# Print the results
+print(counts)
+percentages <- round(prop.table(counts) * 100, 2)
+print(percentages)
+
+counts2 <- table(df.tmp$converted_008_33[df.tmp$data_element_008 == "Kirjat"], useNA = 'always')
+# Calculate percentages
+percentages <- round(prop.table(counts2) * 100, 2)
+result_df <- data.frame(
+  data_element = names(counts2),
+  count = counts2,
+  percentage = percentages
+)
+
+result_df <- result_df %>%
+  select(data_element, count.Freq, percentage.Freq)
+
+
+# Filter the original dataframe to include only rows where melinda_id is in melindas_19
+filtered_df <- df.orig[df.orig$melinda_id %in% melindas_19, ]
+
+# Create a summary dataframe with unique data elements, count, and percentage (including NA)
+summary_df <- df.tmp %>%
+  group_by(data_element_008) %>%
+  summarise(
+    count = n(),
+    percentage = (n() / nrow(filtered_df)) * 100
+  )
+
+# Convert to a dataframe if necessary
+summary_df <- as.data.frame(summary_df)
+
+# Display the resulting dataframe
+print(summary_df)
+
+
 
 message("Accepted entries in the preprocessed data")
+i <- write_xtable(df.tmp$data_element_008, file_accepted, count = TRUE, add.percentages = TRUE)
 s <- write_xtable(df.tmp2, file_accepted, count = TRUE, add.percentages = FALSE)
 
 
