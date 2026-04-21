6709c9355d396788e80bf534053a35ccce1ef2aa
Julia <yulmat@utu.fi>
2026-04-21 11:50:54 +0000

    fix connection

--- inst/examples/008_33.R
+++ inst/examples/008_33.R
@@ -24,7 +24,8 @@ write.table(df, file = paste0(output.folder, paste0(field, ".csv")), quote = FAL
 # Generate data summaries for the whole data set
 
 message("Accepted entries in the preprocessed data")
-s <- write_xtable(df_genre_008$converted_008_33, file_accepted, count = TRUE)
+df_genre_008$converted_008_33 <- as.character(df_genre_008$converted_008_33)
+s <- write_xtable(as.character(df_genre_008$converted_008_33), file_accepted, count = TRUE)
 
 message("Discarded entries in the original data")
 
@@ -35,7 +36,7 @@ inds <- which(is.na(df_genre_008$converted_008_33))
 original.na <- df.orig[match(df_genre_008$melinda_id[inds], df.orig$melinda_id), field]
 
 # .. ie. those are "discarded" cases; list them in a table
-tmp <- write_xtable(original.na, file_discarded, count = TRUE)
+tmp <- write_xtable(as.character(original.na), file_discarded, count = TRUE)
 
 
 # ------------------------------------------------------------
@@ -56,24 +57,24 @@ df_19 <- readRDS(data.file)
 write.table(df_19, file = paste0(output.folder, paste0(field, "_19.csv")), quote = FALSE)
 
 
-# Define output files for the 1807-1917 subset
-file_accepted_19  <- paste0(output.folder, field, "_accepted_19.csv")
-file_discarded_19 <- paste0(output.folder, field, "_discarded_19.csv")
-
-# ------------------------------------------------------------
-
-# Generate data summaries for 1809-1917
-message("Accepted entries in the preprocessed data for 1809-1917")
-s <- write_xtable(df_19[[field]], file_accepted_19, count = TRUE)
-
-message("Discarded entries for 1809-1917")
-
-# NA values in the final harmonized data
-inds <- which(is.na(df_19[[field]]))
-
-# Original entries that were converted into NA
-original.na <- df.orig[match(df_19$melinda_id[inds], df.orig$melinda_id), field]
-
-# .. ie. those are "discarded" cases; list them in a table
-tmp19 <- write_xtable(original.na, file_discarded_19, count = TRUE)
-
+# # Define output files for the 1807-1917 subset
+# file_accepted_19  <- paste0(output.folder, field, "_accepted_19.csv")
+# file_discarded_19 <- paste0(output.folder, field, "_discarded_19.csv")
+# 
+# # ------------------------------------------------------------
+# 
+# # Generate data summaries for 1809-1917
+# message("Accepted entries in the preprocessed data for 1809-1917")
+# s <- write_xtable(df_19[[field]], file_accepted_19, count = TRUE)
+# 
+# message("Discarded entries for 1809-1917")
+# 
+# # NA values in the final harmonized data
+# inds <- which(is.na(df_19[[field]]))
+# 
+# # Original entries that were converted into NA
+# original.na <- df.orig[match(df_19$melinda_id[inds], df.orig$melinda_id), field]
+# 
+# # .. ie. those are "discarded" cases; list them in a table
+# tmp19 <- write_xtable(original.na, file_discarded_19, count = TRUE)
+# 
--- inst/examples/priority_fields_kanto.R
+++ inst/examples/priority_fields_kanto.R
@@ -1,9 +1,9 @@
 library(arrow)
 library(dplyr)
 
-url <- "https://a3s.fi/swift/v1/AUTH_3c0ccb602fa24298a6fe3ae224ca022f/fennica-container/fennica_kanto_enriched.parquet"
+url <- "https://a3s.fi/swift/v1/AUTH_3c0ccb602fa24298a6fe3ae224ca022f/fennica-container/fennica_enriched.parquet"
 
-df.kanto <- read_parquet("fennica_enriched.parquet")
+df.kanto <- read_parquet(url)
 
 df.kanto <- df.kanto %>%
   mutate(author_profession_kanto_fi = profession_metadata_profession_prefLabel_fi)
